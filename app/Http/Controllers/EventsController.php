cc43addde8e6ce5dbba378d3c4354ba080b2cd22
Vinod Bhatia <vinodbhatia21@gmail.com>
2021-05-20 19:30:15 +0530

    future events with workshop

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Date;
+use DB;
 
 class EventsController extends BaseController
 {
@@ -177,6 +178,9 @@ class EventsController extends BaseController
      */
 
     public function getFutureEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 2');
+        $futureEventsWithWorkshop =  Event::with('workshops')->whereHas('workshops', function ($query) {
+            $query->where('start', '>', date('Y-m-d H:i:s'));
+        })->get();
+        return response($futureEventsWithWorkshop, 200);
     }
 }
